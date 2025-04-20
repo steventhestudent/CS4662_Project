34db5f4b6648b948679b6451ba0b212768f58bbf
steventhestudent <sgonza272@calstatela.edu>
2025-04-20 05:55:24 -0700

    Data Exploration — update

--- DevelopedMethods/imports.py
+++ DevelopedMethods/imports.py
@@ -10,12 +10,13 @@ from sklearn.cluster import KMeans # K-means Clustering
 from sklearn.naive_bayes import GaussianNB # naive bayes
 
 # sklearn misc.
-from sklearn.preprocessing import StandardScaler, LabelEncoder        # StandardScaler = normalize features             ——LabelEncoder =  Label Encoding (convert value into unique int) - good for ft. w/ many unique values (to not create many dummy columns)
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler        # StandardScaler = normalize features             ——LabelEncoder =  Label Encoding (convert value into unique int) - good for ft. w/ many unique values (to not create many dummy columns)
 from sklearn import metrics                                           # accuracy score
 from sklearn.model_selection import cross_val_score, train_test_split # cross_val_score = average(repeatedly split dataset into training/testing, .fit(), accuracy_score(.predict())         ——train_test_split example:   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=x)
 from sklearn.pipeline import make_pipeline                            #
 from sklearn.datasets import make_blobs
 from sklearn.decomposition import PCA # (principal component analysis (unsupervised dimensionality reduction))
+from sklearn.manifold import TSNE # PCA alternative (we use for SMOTE noise detection in tight clusters)
 
 # misc.
 from imblearn.over_sampling import SMOTE
