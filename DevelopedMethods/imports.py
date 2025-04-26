ef93a6ec12faf9abc74607b9e6a4a8819894a869
steventhestudent <sgonza272@calstatela.edu>
2025-04-26 09:30:56 -0700

    last semester + data explore update

--- DevelopedMethods/imports.py
+++ DevelopedMethods/imports.py
@@ -12,7 +12,7 @@ from sklearn.naive_bayes import GaussianNB # naive bayes
 # sklearn misc.
 from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler        # StandardScaler = normalize features             ——LabelEncoder =  Label Encoding (convert value into unique int) - good for ft. w/ many unique values (to not create many dummy columns)
 from sklearn import metrics                                           # accuracy score
-from sklearn.model_selection import cross_val_score, train_test_split # cross_val_score = average(repeatedly split dataset into training/testing, .fit(), accuracy_score(.predict())         ——train_test_split example:   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=x)
+from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, GridSearchCV # cross_val_score = average(repeatedly split dataset into training/testing, .fit(), accuracy_score(.predict())         ——train_test_split example:   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=x)
 from sklearn.pipeline import make_pipeline                            #
 from sklearn.datasets import make_blobs
 from sklearn.decomposition import PCA # (principal component analysis (unsupervised dimensionality reduction))
